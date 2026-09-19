cd97a2e95af17eaf7a657173156a5d02b4b54ac2
mdraza <raza.uidev@gmail.com>
2026-09-19 19:15:51 +0530

    exploring get, post and delete

--- src/app.js
+++ src/app.js
@@ -2,16 +2,24 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hi...Node JS");
+app.get("/user", (req, res) => {
+  res.send({ fname: "Umamah", lname: "Raza" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data saved into the database successfully!");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User data deleted successfully!");
+});
+
+app.use("/user", (req, res) => {
+  res.send("Waaah kya baat hai....janab");
 });
 
 app.use("/test", (req, res) => {
   res.send("Hi...Test");
-});
-
-app.use("/demo", (req, res) => {
-  res.send("Demo...");
 });
 
 app.listen(3000, () => {
