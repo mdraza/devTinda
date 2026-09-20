edc0a5a0bafa94ed958e4feae4a8d87aa0756481
mdraza <raza.uidev@gmail.com>
2026-09-20 15:36:47 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,25 +2,24 @@ import express from "express";
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ fname: "Umamah", lname: "Raza" });
-});
-
-app.post("/user", (req, res) => {
-  res.send("Data saved into the database successfully!");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("User data deleted successfully!");
-});
-
-app.use("/user", (req, res) => {
-  res.send("Waaah kya baat hai....janab");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hi...Test");
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    // res.send("Request handler-1");
+    next();
+  },
+  (req, res, next) => {
+    // res.send("Request handler-2");
+    next();
+  },
+  (req, res, next) => {
+    // res.send("Request handler-3");
+    next();
+  },
+  (req, res, next) => {
+    res.send("Request handler-4");
+  },
+);
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
